7d5a58f53898802c0396683806bdc37b9e61068f
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2024-03-07 14:57:03 +0700

    feat: Add GET, POST, PUT, DELETE endpoints for seat

---
+++ routes/seat.routes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const Auth = require("../middlewares/authentication");
+const checkRole = require("../middlewares/checkRole");
+const { getAllSeatsByFlightId, createManySeats, reserveSeatById, deleteManySeatsByFlightId } = require("../controllers/seat.controllers");
+
+router.post("/", Auth, checkRole(["ADMIN"]), createManySeats);
+router.get("/:flightId", Auth, checkRole(["USER", "ADMIN"]), getAllSeatsByFlightId);
+router.put("/:seatId", Auth, checkRole(["USER", "ADMIN"]), reserveSeatById);
+router.delete("/:flightId", Auth, checkRole(["ADMIN"]), deleteManySeatsByFlightId);
+
+module.exports = router;
