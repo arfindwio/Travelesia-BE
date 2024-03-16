d220b34701e9f29ce179e400bd905adb20c0ceca
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2024-03-17 01:02:02 +0700

    feat: update promotionCheck function with node-cron

--- utils/cronJobs.js
+++ utils/cronJobs.js
@@ -3,14 +3,14 @@ const prisma = require("../libs/prismaClient");
 
 module.exports = {
   promotionCheck: () => {
-    cron.schedule("0 0 * * *", async function () {
+    // cron.schedule("0 0 * * *", async function () {
+    cron.schedule("* * * * *", async function () {
       const flights = await prisma.flight.findMany();
+      const promotions = await prisma.promotion.findMany();
       const validPromotionIds = flights.map((data) => data.promotionId).filter((promotionId) => promotionId !== null);
 
       for (const promotionId of validPromotionIds) {
-        const promotion = await prisma.promotion.findUnique({
-          where: { id: promotionId },
-        });
+        const promotion = promotions.find((promotion) => promotion.id === promotionId);
 
         if (!promotion || new Date(promotion.endDate) < new Date()) {
           const flightsToUpdate = flights.filter((flight) => flight.promotionId === promotionId);
